e4b0e2bd188c7ab911538d8fcb18155a99aadbc9
eliza-gabriela <161747069+eliza-gabriela@users.noreply.github.com>
2024-05-18 10:36:09 -0300

    Add files via upload

---
+++ login.js
@@ -0,0 +1,7 @@
+const f_email=document.querySelector("#f_email");
+const f_senha=document.querySelector("#f_senha");
+const btn_login=document.querySelector("#btn_login");
+
+btn_login.addEventListener("click",(evt)=>{
+    window.location.href="./almoxarifado.html";
+})
